8fb92befa88b22def92979850bbd485ced73f2e5
Rob Brennan <rob@therobbrennan.com>
2020-06-20 21:50:25 -0700

    Defined ADD_TODO mutation

--- app-boilerplate/components/Todo/TodoInput.js
+++ app-boilerplate/components/Todo/TodoInput.js
@@ -1,10 +1,25 @@
 import React from "react";
+import gql from "graphql-tag";
+
+export const ADD_TODO = gql`
+  mutation($todo: String!, $isPublic: Boolean!) {
+    insert_todos(objects: { title: $todo, is_public: $isPublic }) {
+      affected_rows
+      returning {
+        id
+        title
+        created_at
+        is_completed
+      }
+    }
+  }
+`;
 
 const TodoInput = ({ isPublic = false }) => {
   return (
     <form
       className="formInput"
-      onSubmit={e => {
+      onSubmit={(e) => {
         e.preventDefault();
       }}
     >
